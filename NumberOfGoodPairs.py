1ae08ea4b9c5d054ba4bfe673d251582014a2bd6
yellyB <kk3p1q1@gmail.com>
2021-07-12 21:19:00 +0900

    1512. Number of Good Pairs

--- NumberOfGoodPairs.py
+++ NumberOfGoodPairs.py
@@ -1,8 +1,15 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        goodPairsCnt = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    goodPairsCnt += 1
-        return goodPairsCnt
+        cnt = 0
+        cntOfNum = {}
+        for num in nums:
+            if num in cntOfNum:
+                cntOfNum[num] += 1
+            else:
+                cntOfNum[num] = 1
+
+        values = cntOfNum.values()
+        for val in values:
+            if val > 1:
+                cnt = cnt + (val * (val - 1) // 2)
+        return cnt
