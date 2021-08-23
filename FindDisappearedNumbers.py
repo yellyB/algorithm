3a622e19871884e59f21bb7251caa40a9b9a1396
yellyB <kk3p1q1@gmail.com>
2021-08-23 11:09:00 +0900

    refact) 448. Find All Numbers Disappeared in an Array

--- FindDisappearedNumbers.py
+++ FindDisappearedNumbers.py
@@ -1,11 +1,12 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        length = len (nums)
-        boolList = [False]*(length+1)
+        length = len(nums)
+        isExist = [False] * (length + 1)  # 0값은 nums없지만 인덱스=숫자 를 맞추기 위해 한 칸 더 만듬
         answer = []
-        for n in nums:
-            boolList[n]=True
-        for i in range (1,length+1):
-            if not boolList[i]:
-                answer.append (i)
+        for num in nums:
+            isExist[num] = True
+
+        for i in range(1, length + 1):
+            if isExist[i] == False:
+                answer.append(i)
         return answer
