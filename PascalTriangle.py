781a5d61bc6df4cbfc1e6554c1e876ed028c3cf3
yellyB <kk3p1q1@gmail.com>
2021-10-29 17:40:00 +0900

    refact) 118. Pascal's Triangle

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,13 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        answer = []
-        for row in range(numRows):
-            tempRow = []
-            for i in range(0, row):
-                if i == 0:
-                    tempRow.append(1)  # 0번째에 1붙임
-                else:
-                    tempRow.append(answer[row-1][i-1] + answer[row-1][i])
-            tempRow.append(1)  # 마지막에 1붙임
-            answer.append(tempRow)
-        return answer
+        rows = [[1] * (i + 1) for i in range(numRows)]  # 하나씩 늘어나면서 1로 들어가게 초기화해줌
+        for i in range(2, len(rows)):
+            for j in range(1, i):
+                rows[i][j] = rows[i - 1][j - 1] + rows[i - 1][j]
+        return rows
+
+    # 아니 풀었었는데 왜 풀었다고 체크 안됨? ㅡㅡ
+    # 개선해서 다시 풂
