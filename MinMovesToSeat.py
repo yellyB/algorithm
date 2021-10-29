e34253e39f5fc2f711171a3f524bf9401fef2427
yellyB <kk3p1q1@gmail.com>
2021-10-29 18:59:00 +0900

    2037. Minimum Number of Moves to Seat Everyone

---
+++ MinMovesToSeat.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        print(seats, students)
+        cnt = 0
+        for i in range(len(seats)):
+            cnt += abs(seats[i] - students[i])
+        return cnt
