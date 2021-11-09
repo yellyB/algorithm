4c59ad4846718e2269304b0579bb395bdeaffc6f
yellyB <kk3p1q1@gmail.com>
2021-11-09 11:23:00 +0900

    refact) 1. Two Sum

--- TwoSum.py
+++ TwoSum.py
@@ -1,7 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            rest = target - nums[i]
-            for j in range(i+1, len(nums)):
-                if nums[j] == rest:
-                    return [i,j]
+        restDict = {}
+        for i, num in enumerate(nums):
+            rest = target - num
+            if rest in restDict:
+                return [restDict[rest], i]
+            else:
+                restDict[num] = i
+
+        # nums돌면서 타겟에서 숫자를 빼서 그 나머지값이랑 인덱스 저장
