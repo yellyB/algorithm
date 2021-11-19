99120212dabf137a3d1851813fe03ae0ac493492
yellyB <kk3p1q1@gmail.com>
2021-11-19 18:06:00 +0900

    refact) 94. Binary Tree Inorder Traversal

--- BinaryTreeInorderTraversal.py
+++ BinaryTreeInorderTraversal.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        def getInorder(root):
-            inorder = []
+        stack, result = [], []
+        node = root
+        while node or stack:
+            while node:  # 왼쪽으로 먼저 탐색하며 서브노드를 계속 누적
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            result.append(node.val)
+            node = node.right  # 오른쪽 값. fullnode면 젤 첨에는 null일거임
+        return result
 
-            if root.left:
-                inorder.extend(getInorder(root.left))  # left가 있으면 추가
-            inorder.extend([root.val])  # 두번째 순서인 val추가(val은 무조건 있으니)
-            if root.right:
-                inorder.extend(getInorder(root.right))  # right도 있다면 추가
-            return inorder
-
-        if not root:
-            return []
-
-        return getInorder(root)
+    # 중위 순회(Inorder)은 다음의 순서로 진행된다.
+    # 왼쪽 서브 트리를 중위 순회한다.
+    # 노드를 방문한다.
+    # 오른쪽 서브 트리를 중위 순회한다.
