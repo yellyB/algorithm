61b8b51c437be9249f06805c7577d0d81f12f35d
yellyB <kk3p1q1@gmail.com>
2021-11-21 14:01:00 +0900

    refact) 136. Single Number

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,10 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        temp = []  # 요소를 저장, 같은 값의 요소가 있으면 제거할거임
-        for num in nums:
-            if num in temp:
-                temp.remove(num)
-            else:
-                temp.append(num)
+        ans = 0
+        for n in nums:
+            ans ^= n
+        return ans
 
-        return temp[0]
+    # c++로도 품
