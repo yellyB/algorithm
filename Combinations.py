c36c14ece50225d8bee601597404efd812a85666
yellyB <kk3p1q1@gmail.com>
2021-11-21 19:09:00 +0900

    77. Combinations

---
+++ Combinations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if k == 0:
+            return [[]]
+        return [pre + [i] for i in range(k, n + 1) for pre in self.combine(i - 1, k - 1)]
+
+    # 통과용
