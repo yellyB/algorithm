16ee640e6f1cb2a01a92160ca12acbfff6685c33
yellyB <kk3p1q1@gmail.com>
2021-12-22 11:41:26 +0900

    refact) 832. Flipping an Image

--- FlippingImage.py
+++ FlippingImage.py
@@ -1,13 +1,8 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        colLen = len(image[0])
         for i in range(len(image)):
-            colLen = len(image[0])
-            # 좌우반전
-            for j in range(colLen//2):
-                image[i][j], image[i][colLen-j-1] = image[i][colLen-j-1], image[i][j]
-            # 값 반전
-            for j in range(len(image[0])):
-                # image[i][j] = 1 if image[i][j] == 0 else 1
-                if image[i][j] == 0: image[i][j] = 1
-                else: image[i][j] = 0
+            image[i].reverse()  # 좌우반전
+            for j in range(colLen):  # 값 반전
+                image[i][j] = 1 if image[i][j] == 0 else 0
         return image
