ad22dc5821ad575679922252894f1cefd51429ba
yellyB <kk3p1q1@gmail.com>
2022-02-15 10:32:30 +0900

    344. Reverse String

---
+++ ReverseString.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let l = 0,
+    r = s.length - 1;
+  while (l < r) {
+    const temp = s[l];
+    s[l] = s[r];
+    s[r] = temp;
+    l++;
+    r--;
+  }
+  return s;
+};
+
+const s = ["h", "e", "l", "l", "o"];
+console.log(reverseString(s));
