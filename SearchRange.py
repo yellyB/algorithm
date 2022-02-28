6dbb4a6ee0c7442e3b93d59681b747a53488febb
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-02-28 17:38:27 +0900

    refactor : 34. Find First and Last Position of Element in Sorted Array

--- SearchRange.py
+++ SearchRange.py
@@ -1,10 +1,21 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        s = -1
-        e = -1
-        for i, n in enumerate(nums):
-            if n == target:
-                if s == -1: s = i  # target을 최초로 발견했을때만 s에 저장
-                e = i  # e에는 target을 찾을때마다 저장
-                
+        def binarySearch(nums, x):
+            left, right = 0, len(nums)
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] < x: left = mid + 1
+                else: right = mid
+            return right
+        
+        
+        s = binarySearch(nums, target)
+        e = binarySearch(nums, target+1) - 1
+        
+        if s > e: return [-1, -1]  # 못찾았으면
+        
         return [s, e]
+    
+    # - target보다 1 큰 수의 위치 찾아서 -1하면 target이 위치한 가장 마지막 위치임
+    # - 이진탐색할때 중복되는 숫자들에서 첫째 위치 찾으려면
+    #   왼, 오 두 포인터 비교해서 while문 끝까지 돌림 됨
