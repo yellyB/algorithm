a9efc0e2014b3e0b3af625098dd5598154735d90
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-03-19 12:34:22 +0900

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,21 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [nums]
-        nums.sort()
         answer = []
-        answer.append(nums[:])
-        while True:
-            i = len(nums) - 2
-            while nums[i] > nums[i+1]:  # 뒤 숫자가 큰게 나올때까지 i--
-                i -= 1
-                if i == -1:  # nums가 내림차순 정렬되었단 뜻. 함수 종료
-                    return answer
-            j = len(nums) - 1
-            while j > i:
-                if nums[i] < nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    nums = nums[:i+1] + sorted(nums[i+1:])
-                    answer.append(nums[:])
-                    break
-                j -= 1
+        def func(idx):
+            if idx == len(nums): answer.append(nums[:])
+            for i in range(idx, len(nums)):
+                nums[idx], nums[i] = nums[i], nums[idx]
+                func(idx+1)
+                nums[idx], nums[i] = nums[i], nums[idx]
+        nums.sort()
+        func(0)
+        return answer
