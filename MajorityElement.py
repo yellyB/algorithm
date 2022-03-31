74e104d546afc1c60f1ba052091af7f1f1293ac7
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-03-31 23:32:34 +0900

    169. Majority Element

---
+++ MajorityElement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq = {}
+        for n in nums:
+            if n in freq: freq[n] += 1
+            else: freq[n] = 1
+        
+        sort = sorted(freq.items(), key=lambda x:x[1], reverse=True)
+        
+        
+        return sort[0][0]
