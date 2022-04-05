bc6a9f64a5f704dc5f26d5407466648632f8691c
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-04-05 21:49:21 +0900

    [js] 208. Implement Trie (Prefix Tree)

---
+++ ImplementTrie.js
@@ -0,0 +1,42 @@
+
+var Trie = function() {
+    this.trie = [];
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    this.trie.push(word);
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    for(const t of this.trie){
+        if(t == word) return true;
+    }
+    return false;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    for(const t of this.trie){
+        if(prefix == t.slice(0, prefix.length)) return true;
+    }
+    return false;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
