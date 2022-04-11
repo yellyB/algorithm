df2597d1c53e8cf6e60be5bd5c1e08a003827c9c
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-04-12 00:15:44 +0900

    Update and rename temp.py to LowestCommonAncestor2.py

--- LowestCommonAncestor2.py
+++ LowestCommonAncestor2.py
@@ -13,14 +13,16 @@ class Solution:
             left = recursion(root.left)
             right = recursion(root.right)
             
-            mid = root == p or root == q
+            # 밑에서 리턴할때 root를 리턴하기때문에(다른건 boolean형태)
+            # TreeNode를 리턴했을경우(정답인경우)는 따로 처리해줌
+            if type(left) == TreeNode: return left
+            if type(right) == TreeNode: return right
             
-            if mid + left + right >= 2:
+            mid = root == p or root == q  # 자기 자신이 조상일수도있기때문
+            
+            if mid + left + right >= 2:  # 세가지 경우중 2개가 true여야함
                 return root
             
-            return mid or left or right
+            return mid or left or right  # 하나라도 True면
             
-            return
-        
-        
         return recursion(root)
