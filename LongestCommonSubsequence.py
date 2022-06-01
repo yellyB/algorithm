1c55425eafb4886c843acfd0b84a658eca917b43
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-06-01 17:43:22 +0900

    Update LongestCommonSubsequence.py

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -1,16 +1,31 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = {}
         n, m = len(text1), len(text2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
         
-        def top_down(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i>=n or j>=m:
-                return 0
-            if text1[i] == text2[j]:
-                return top_down(i+1, j+1) + 1
-            memo[(i, j)] = max(top_down(i, j+1), top_down(i+1, j))
-            return memo[(i, j)]
+        for i in range(n):
+            for j in range(m):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
         
-        return top_down(0, 0)
+        return dp[-1][-1]
+        
+        
+        
+#        [top down]
+#         memo = {}
+#         n, m = len(text1), len(text2)
+        
+#         def top_down(i, j):
+#             if (i, j) in memo:
+#                 return memo[(i, j)]
+#             if i>=n or j>=m:
+#                 return 0
+#             if text1[i] == text2[j]:
+#                 return top_down(i+1, j+1) + 1
+#             memo[(i, j)] = max(top_down(i, j+1), top_down(i+1, j))
+#             return memo[(i, j)]
+        
+#         return top_down(0, 0)
