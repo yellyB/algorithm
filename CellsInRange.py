8e318ef8c2512a70a623e7a68a4019b17b2e25a5
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-06-16 17:25:35 +0900

    2194. Cells in a Range on an Excel Sheet

---
+++ CellsInRange.py
@@ -0,0 +1,7 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        res = []
+        for i in range(ord(s[0]), ord(s[3])+1):  # 문자열의 아스키코드 이용
+            for j in range(int(s[1]), int(s[4])+1):  # 숫자 범위를 돌림
+                res.append(chr(i) + str(j))
+        return res
