b69516dc877f9dc131e6ac5c9c2b4316f02cf5ea
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-06-22 17:50:11 +0900

    Update EscapeTheSpreadingFire.py

--- EscapeTheSpreadingFire.py
+++ EscapeTheSpreadingFire.py
@@ -1,7 +1,6 @@
 class Solution:
     def maximumMinutes(self, originGrid: List[List[int]]) -> int:
-        if originGrid == [[0,0,0,0,0],[0,2,0,2,0],[0,2,0,2,0],[0,2,1,2,0],[0,2,2,2,0],[0,0,0,0,0]]:
-            return 1
+        MAX_VALUE = 1000000000
         fires = []
         grid = []
         m, n = len(originGrid), len(originGrid[0])
@@ -36,43 +35,39 @@ class Solution:
         # 2. 출구까지 가면서
         #    그 루트에 불이 퍼지는 시간과 그 장소까지 가는데 걸린 시간을 이용해
         #    최대 기다릴 수 있는 시간 구하기
-        memo = [[0]*n for _ in range(m)]
-        visited = [[False] * n for _ in range(m)]
+        memo = [[-1]*n for _ in range(m)]
         times = 1
         q = []
-        heapq.heappush(q, (1,0,0))  # (step, row, col)  내 위치까지 불이 오려면 1초전에 나가야 하니까 기다리는 시간을 -1 해줌.
-        isSafeHouse = False
-        isMid = False  # 중간에 불에 쫓기는 순간 있나
-        while q:
-            curr = heapq.heappop(q)
-            step, r, c = curr[0], curr[1], curr[2]
-            for row, col in [[r,c+1],[r+1,c],[r,c-1],[r-1,c]]:
-                if 0<=row and row<m and 0<=col and col<n and not visited[row][col] and grid[row][col]!='/':
-                    isEnd = row == m-1 and col == n-1
-                    
-                    if step<grid[row][col] or (step==grid[row][col] and row==m-1 and col==n-1):
-                        memo[row][col] = max( memo[row][col], grid[row][col]-step )
-                        if row==m-1 and col==n-1 and step+1 == grid[row][col]:
-                            isSafeHouse = True
-                        elif row<m-1 and col<n-1 and step+1 == grid[row][col]:
-                            isMid = True
-                        heapq.heappush(q, (step+1, row, col))
-                    
-            visited[r][c] = True
-            times += 1
-        # print(memo)
-        # print(visited)
-        # print(isSafeHouse, isMid)
-        res = memo[-1][-1] -1
-        
-        if 1000000000 <= res:
-            return 1000000000
-        if not visited[-1][-1]:  # 마지막칸 방문 못했으면
-            return -1
-        elif visited[-1][-1] and memo[-1][-1] == 0:  # 방문은 했는데 불이랑 같이 도착한 경우(겨우세이프)
-            return 0
+        if 0<=grid[0][0]:
+            memo[0][0] = grid[0][0]-1
         else:
-            if not isSafeHouse and not isMid:
-                return res
-            else:
-                return res+1
+            memo[0][0] = MAX_VALUE
+            
+        heapq.heappush(q, (memo[0][0],0,0))
+        
+        while q:
+            temp = []
+            for _ in range(len(q), 0, -1):  # 추가되는애들 말고 현재 있는 큐만 일단 검사하기 위해(한번 돌때마다 times 증가)
+                curr = heapq.heappop(q)
+                delay, r, c = curr[0], curr[1], curr[2]
+                for row, col in [[r,c+1],[r+1,c],[r,c-1],[r-1,c]]:
+                    if 0<=row and row<m and 0<=col and col<n and grid[row][col]!='/':
+                        isNotSafeHouse = row != m-1 or col != n-1  # safe house면 불 만나도 집에 들어가면 되니까 1초의 추가 여유 주기 위해
+                        if 0 <= grid[row][col]:
+                            maxPossibleDelay = grid[row][col] - times - int(isNotSafeHouse)
+                        else:
+                            maxPossibleDelay = MAX_VALUE
+                        nextDelay = min(delay, maxPossibleDelay)
+                        visited = 0<=memo[row][col]
+
+                        if nextDelay<0 or (visited and nextDelay <= memo[row][col]):
+                            continue
+                        memo[row][col] = nextDelay
+                        # q.append((nextDelay, row, col))  # 힙푸시 안하는 이유: 위에서 for문으로 q동안 돌도록 하는데 히힙푸시하면 순서가 바껴버린다!
+                        heapq.heappush(temp, (nextDelay, row, col))
+            q = temp[:]
+            times += 1
+            
+        # print(memo)
+        
+        return memo[-1][-1] if memo[-1][-1] != float('inf') else MAX_VALUE
