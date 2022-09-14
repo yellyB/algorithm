456415bca9f5829298abb0582b2f6679ac869a21
bomiKim <50893303+yellyB@users.noreply.github.com>
2022-09-14 22:28:34 +0900

    Update MinimumTimeDifference.py

--- MinimumTimeDifference.py
+++ MinimumTimeDifference.py
@@ -9,7 +9,7 @@ class Solution:
             vals.append(convertToMinite(tp))
         vals.sort()
         # print(vals)
-        minDiff = 1440 - vals[-1] + vals[0]  # 가장 마지막도 첫 값이랑 비교해주기 위해. 거꾸로 가는 diff를 계산
+        minDiff = convertToMinite("24:00") - vals[-1] + vals[0]  # 가장 마지막도 첫 값이랑 비교해주기 위해. 거꾸로 가는 diff를 계산
         for i in range(len(vals)):
             minDiff = min(minDiff, abs(vals[i] - vals[i-1]))
             
