634d1777f44718679505e48fcd84173df62b607c
bomiKim <kk3p1q1@gmail.com>
2023-01-15 00:30:44 +0900

    2535. Difference Between Element Sum and Digit Sum of an Array

--- DifferenceBetweenElementSumAndDigitSumOfArray.py
+++ DifferenceBetweenElementSumAndDigitSumOfArray.py
@@ -1,6 +1,16 @@
 class Solution:
     def differenceOfSum(self, nums: List[int]) -> int:
-        sumOfElement = sum(nums)
-        sumOfDigits = sum(map(int,''.join(map(str, nums))))
+        # 이게 더 빠름
+        res = 0
+        for num in nums:
+            res += num
+            while num != 0:
+                res -= abs(num%10)
+                num //= 10
 
-        return abs(sumOfElement - sumOfDigits)
+        return res
+
+        # sumOfElement = sum(nums)
+        # sumOfDigits = sum(map(int,''.join(map(str, nums))))
+
+        # return abs(sumOfElement - sumOfDigits)
