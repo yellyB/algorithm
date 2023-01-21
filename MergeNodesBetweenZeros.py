be932cd025fcfb15bd63896cd5f10fc98c46b420
bomiKim <50893303+yellyB@users.noreply.github.com>
2023-01-22 00:25:47 +0900

    2181. Merge Nodes in Between Zeros

--- MergeNodesBetweenZeros.py
+++ MergeNodesBetweenZeros.py
@@ -5,17 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        temp = temp.next
-        curr = head
+        p1 = head  # 구한 합계를 업뎃하기 위한 포인터(slow)
+        p2 = head.next  # 노드 순회해가면서 합계에 저장하기 위한 포인터(fast)
         sums = 0
-        while temp:
-            if temp.val:
-                sums += temp.val
+        while p2:
+            if p2.val:
+                sums += p2.val
             else:
-                curr = curr.next
-                curr.val = sums
+                p1 = p1.next
+                p1.val = sums
                 sums = 0
-            temp = temp.next
-        curr.next = None
+            p2 = p2.next
+        p1.next = None
         return head.next
