c4c7130125c8757859e2b956855df8892c789634
bomiKim <50893303+yellyB@users.noreply.github.com>
2023-02-21 19:51:34 +0900

    2103. Rings and Rods

--- RingsAndRods.py
+++ RingsAndRods.py
@@ -8,7 +8,7 @@ class Solution:
             dict[idx].add(color)
 
         for val in dict.values():
-            if 'R' in val and 'G' in val and 'B' in val:
+            if 'R' in val and 'G' in val and 'B' in val:  # 이 부분 더 좋게 바꿀 수 있을 것 같은데...
                 res += 1
         
         return res
